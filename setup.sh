72b0ade22a6de34ac87cf2e57c211dfc63bf8de6
Conke Hu <conke.hu@gmail.com>
2018-05-02 00:43:22 +0800

    misc update
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+for g in Comcast.sdkman fubarhouse.nodejs; do
+  for (( i = 0; i < 5; i++ )); do
+    ansible-galaxy install $g && break
+  done
+done
+
+pyexec=`which python3`
+if [ -z "$pyexec" ]; then
+    pyexec=`which python`
+fi
+
+cp hosts.sample hosts
+cat >> hosts << __EOF__
+[master:vars]
+ansible_python_interpreter=$pyexec
+__EOF__
+
+ansible-playbook -i hosts site.yml
