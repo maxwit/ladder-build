9b92aa0c3d1911005d94808ea3ce90216e948813
Conke Hu <conke.hu@gmail.com>
2018-05-03 14:37:15 +0800

    update setup.sh
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-pip install --user ansible
+pip install --user ansible || exit 1
 
-for g in Comcast.sdkman fubarhouse.nodejs; do
+for g in `grep '^\s\+- role:' site.yml | awk '{print $3}'`; do
   for (( i = 0; i < 5; i++ )); do
     ansible-galaxy install $g && break
   done
 done
 
-pyexec=`which python3`
+pyexec=`which python`
 if [ -z "$pyexec" ]; then
-    pyexec=`which python`
+    pyexec=`which python3`
 fi
 
 cp hosts.sample hosts
